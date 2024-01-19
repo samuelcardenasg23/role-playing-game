8d68a80b3fd9ea0414cac2e3aa758cec9067fed0
Samuel Cardenas <samuelcardenas1706@gmail.com>
2024-01-18 19:45:42 -0500

    Fighting UI done and fighting buttons on the work.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -67,6 +67,12 @@ const locations = [{
     'button text': ['Fight slime', 'Fight fanged beast', 'Go to town square'],
     'button functions': [fightSlime, fightBeast, goTown],
     text: 'You enter the cave. You see some monsters.'
+},
+{
+    name: 'fight',
+    'button text': ['Attack', 'Dodge', 'Run'],
+    'button functions': [attack, dodge, goTown],
+    text: 'You are fighting a monster.'
 }];
 
 // initialize buttons
@@ -146,13 +152,37 @@ function sellWeapon () {
 }
 
 function fightSlime () {
-
+    fighting = 0;
+    goFight();
 }
 
 function fightBeast () {
-
+    fighting = 1;
+    goFight();
 }
 
 function fightDragon () {
-    console.log('Fighting dragon.');
+    fighting = 2;
+    goFight();
+}
+
+function goFight () {
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = 'block';
+
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsters[fighting].health;
+}
+
+function attack () {
+    text.innerText = 'The ' + monsters[fighting].name + ' attacks.';
+    text.innerText += ' You attack it with your ' + weapons[currentWeapon].name + '.';
+
+    health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeapon].power;
+}
+
+function dodge () {
+
 }
