e5a58d3bea84bcbcc0736342a9317cc541b5e5e6
Samuel Cardenas <samuelcardenas1706@gmail.com>
2024-01-19 00:48:36 -0500

    Font family changed to roboto.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -76,7 +76,7 @@ const locations = [{
 },
 {
     name: 'kill monster',
-    'button text': ['Go to town square', 'Go to town square', 'Go to town square'],
+    'button text': ['Go to town square', 'Go to town square', 'Easter egg'],
     'button functions': [goTown, goTown, easterEgg],
     text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
 },
