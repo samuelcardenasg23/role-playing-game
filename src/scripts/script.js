4229321c263df73463d6785706da4aa9ab6600ae
Samuel Cardenas <samuelcardenas1706@gmail.com>
2024-01-29 10:40:26 -0500

    Remove console log

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -229,7 +229,6 @@ function attack () {
 
 function getMonsterAttackValue(level) {
     const hit = (level * 5) - (Math.floor(Math.random() * xp));
-    console.log(hit);
     return hit > 0 ? hit : 0; 
 }
 
